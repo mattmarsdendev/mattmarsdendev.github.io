9bdd75f64e00c4562dcaef3621dff4cc62a0af11
mmarsden89 <mmarsden89@gmail.com>
2020-10-13 15:55:45 -0700

    Updates

---
+++ precache-manifest.af7385e6ce8289933f1f7d1d6ba0658b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4783a8bcd67b5d51fad7adba1c3297bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f34986d441a84258e1e6",
+    "url": "/static/css/main.91e43f33.chunk.css"
+  },
+  {
+    "revision": "55636ec2c93cb401a084",
+    "url": "/static/js/2.019e470c.chunk.js"
+  },
+  {
+    "revision": "f34986d441a84258e1e6",
+    "url": "/static/js/main.934d8202.chunk.js"
+  },
+  {
+    "revision": "d862855dc3bdd7195b17",
+    "url": "/static/js/runtime-main.b4d6abd0.js"
+  }
+]);
