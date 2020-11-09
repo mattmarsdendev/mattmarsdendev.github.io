ad76a89aeff2e1858e71f888a9438b3a7f520313
mmarsden89 <mattmarsdendev@gmail.com>
2020-11-08 21:27:44 -0800

    remove create react templating and update to component heirarchy I use

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+import Welcome from "./Components/Welcome/Welcome";
+import Navbar from "./Components/Navbar/Navbar";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [experience, setExperience] = useState(false);
+  const [portfolio, setPortfolio] = useState(false);
+  const [contact, setContact] = useState(false);
+  const [welcome, setWelcome] = useState(true);
+
+  const handleExperience = () => {
+    setExperience(true);
+    setPortfolio(false);
+    setContact(false);
+    setWelcome(false);
+  };
+
+  const handlePortfolio = () => {
+    setPortfolio(true);
+    setExperience(false);
+    setContact(false);
+    setWelcome(false);
+  };
+
+  const handleContact = () => {
+    setContact(true);
+    setPortfolio(false);
+    setExperience(false);
+    setWelcome(false);
+  };
+
+  return (
+    <div className="App">
+      <div className="App-container">
+        <div className="card-container">
+          {welcome && <Welcome />}
+          <Navbar
+            handleExperience={handleExperience}
+            handlePortfolio={handlePortfolio}
+            handleContact={handleContact}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
