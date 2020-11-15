8c9a3c80985fae0b51c0dfbdf3e3fb3902bc3e68
mmarsden89 <mattmarsdendev@gmail.com>
2020-11-14 18:14:05 -0800

    start work on portfolio section

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,7 +1,15 @@
 import "./Portfolio.scss";
 
 const Portfolio = (props) => {
-  return <div className="portfolio-container">hello</div>;
+  return (
+    <div className="portfolio-container">
+      <div className="recent-projects">
+        <h4>Recent Projects</h4>
+        <h4>Trividuh</h4>
+        <h4>National Parks</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
