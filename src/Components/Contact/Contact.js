571b129507fc1e4d320501c279ce90ff17088686
mmarsden89 <mattmarsdendev@gmail.com>
2020-11-14 18:49:54 -0800

    contact layout styling

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,18 +3,30 @@ import "./Contact.scss";
 const Contact = (props) => {
   return (
     <div className="contact-container">
-      <div>Looking to get in touch? Feel free to reach out to me here</div>
       <div className="link-container">
-        <a href="" target="_blank">
+        <a
+          href="https://github.com/mmarsden89"
+          target="_blank"
+          rel="noreferrer"
+        >
           Github
         </a>
-        <a href="" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/matthewjmarsden/"
+          target="_blank"
+          rel="noreferrer"
+        >
           LinkedIn
         </a>
-        <a href="" target="_blank">
+        <a
+          href="mailto:mattmarsdendev@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
           Email
         </a>
       </div>
+      <div>Looking to get in touch? Feel free to reach out to me here</div>
     </div>
   );
 };
