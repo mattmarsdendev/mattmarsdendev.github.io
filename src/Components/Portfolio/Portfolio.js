6606bb66a7d6245a6950b9e7a023acf6c4db0a9f
mmarsden89 <mattmarsdendev@gmail.com>
2020-11-14 18:37:34 -0800

    start work on individual portfolio items

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,4 +1,5 @@
 import "./Portfolio.scss";
+import trividuh from "../../trividuh.png";
 
 const Portfolio = (props) => {
   return (
@@ -7,6 +8,13 @@ const Portfolio = (props) => {
         <h4>Recent Projects</h4>
         <h4>Trividuh</h4>
         <h4>National Parks</h4>
+      </div>
+      <div className="portfolio-item">
+        <img src={trividuh} className="portfolio-image" />
+        <p>
+          Trividuh, the trivia app you've always wanted. Answer questions, earn
+          points and unlock more quetion sets! Built with React
+        </p>
       </div>
     </div>
   );
