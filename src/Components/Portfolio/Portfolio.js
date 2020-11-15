492a9bc8d4ab7ecbd283751085e683bd30f7fc77
mmarsden89 <mattmarsdendev@gmail.com>
2020-11-15 14:03:13 -0800

    add national park to portfolio

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,5 +1,6 @@
 import "./Portfolio.scss";
 import trividuhimg from "../../trividuh.png";
+import nationalimg from "../../nationalparks.png";
 import React, { useState } from "react";
 
 const Portfolio = (props) => {
@@ -9,6 +10,11 @@ const Portfolio = (props) => {
   const handleTrividuh = () => {
     setTrividuh(true);
     setNationalPark(false);
+  };
+
+  const handleNational = () => {
+    setTrividuh(false);
+    setNationalPark(true);
   };
 
   const trividuhHTML = (
@@ -31,6 +37,30 @@ const Portfolio = (props) => {
     </div>
   );
 
+  const nationalHTML = (
+    <div className="portfolio-item">
+      <img src={nationalimg} className="portfolio-image" />
+      <div>
+        <p className="portfolio-desc">
+          A project originally started as a tech challenge, I decided to revamp
+          the idea by building out a more robust backend and adding more
+          functionality while also adding to the front-end experience. National
+          Parks is a database to keep track of the 61 parks that you've already
+          visited. Built with MongoDb, Express, React and Node with
+          authorization handled by Auth0.
+        </p>
+        <div className="portfolio-links">
+          <a href="" target="_blank">
+            deployed app
+          </a>
+          <a href="" target="_blank">
+            front-end repo
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <div className="portfolio-container">
       <div className="recent-projects">
@@ -38,9 +68,12 @@ const Portfolio = (props) => {
         <div className="project-nav" onClick={handleTrividuh}>
           Trividuh
         </div>
-        <div className="project-nav">National Parks</div>
+        <div className="project-nav" onClick={handleNational}>
+          National Parks
+        </div>
       </div>
       {trividuh && trividuhHTML}
+      {nationalPark && nationalHTML}
     </div>
   );
 };
