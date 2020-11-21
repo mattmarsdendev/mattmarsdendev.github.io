a92aa0303dbf6bde0b222ca6ff90250caf5d055a
mmarsden89 <mattmarsdendev@gmail.com>
2020-11-21 12:34:47 -0800

    refactor welcome container

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -1,27 +1,7 @@
 import "./Welcome.scss";
+import Tools from "./Tools";
 
 const Welcome = () => {
-  const tools = [
-    "JavaScript",
-    "Python",
-    "(Postgre)SQL",
-    "React",
-    "Node",
-    "Express",
-    "MongoDb",
-    "AWS",
-    "Git / Github",
-    "HTML",
-    "(S)CSS",
-  ];
-
-  const toolHTML = tools.map((tool, idx) => (
-    <div className="single-tool" key={idx}>
-      <div className="arrow">▹</div>
-      <div>{tool}</div>
-    </div>
-  ));
-
   return (
     <div className="welcome-container">
       <div className="introduction">
@@ -31,10 +11,7 @@ const Welcome = () => {
           experience
         </h3>
       </div>
-      <div className="tools">
-        <h3>Here's some tools I use</h3>
-        <div className="tool-container">{toolHTML}</div>
-      </div>
+      <Tools />
     </div>
   );
 };
