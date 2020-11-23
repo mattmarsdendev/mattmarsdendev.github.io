eb4c3d77dfdc585cdae0760d95caaa477681cf88
mmarsden89 <mattmarsdendev@gmail.com>
2020-11-22 16:57:25 -0800

    add ability to have multiple files in directory

--- src/Components/Welcome/DirectoryItems.js
+++ src/Components/Welcome/DirectoryItems.js
@@ -1,5 +1,12 @@
 const DirectoryItems = {
   "/users/MattMarsden/AboutMe/": ["AboutMe.txt"],
+  "/users/MattMarsden/Contact": ["mattmarsdendev@gmail.com"],
+  "/users/MattMarsden/Portfolio": [
+    "mmarsden89.github.io/#/portfolio",
+    "portfolioWebsite.txt",
+    "trividuh.txt",
+    "nationalParks.txt",
+  ],
 };
 
 export default DirectoryItems;
--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -27,6 +27,15 @@ const Welcome = () => {
       const updated = [...terminalReturn, `$ ${inputText}`];
       setTerminalReturn([...updated, DirectoryItems[workingDirectory]]);
       setInputText("");
+    } else if (inputText === "--help") {
+      const help =
+        "Hey, thanks for trying out the terminal! Current working commands: cd, pwd, ls, clear are all functioning. Type 'home' to return to home directory";
+      setTerminalReturn([...terminalReturn, help]);
+      setInputText("");
+    } else if (inputText === "home") {
+      setWorkingDirectory("/users/MattMarsden");
+      setTerminalReturn([...terminalReturn, "$ cd /users/MattMarsden"]);
+      setInputText("");
     } else {
       setTerminalReturn([
         ...terminalReturn,
@@ -47,21 +56,62 @@ const Welcome = () => {
     setTerminalReturn([...ls, `${DirectoryItems[updatedDirectory]}`]);
   };
 
+  const handleContactLink = () => {
+    const updatedDirectory = "/users/MattMarsden/Contact";
+    setWorkingDirectory(updatedDirectory);
+    const cd = [...terminalReturn, `$ cd ${workingDirectory}`];
+    const ls = [...cd, "$ ls"];
+    setTerminalReturn([...ls, `${DirectoryItems[updatedDirectory]}`]);
+  };
+
+  const handlePortfolioLink = () => {
+    const updatedDirectory = "/users/MattMarsden/Portfolio";
+    setWorkingDirectory(updatedDirectory);
+    const cd = [...terminalReturn, `$ cd ${workingDirectory}`];
+    const ls = [...cd, "$ ls"];
+    const updated = [...terminalReturn];
+    DirectoryItems[updatedDirectory].map((item) => {
+      updated.push(item);
+      setTerminalReturn(updated);
+    });
+  };
+
   const handleChange = (e) => {
     setInputText(e.target.value);
     console.log(inputText);
   };
 
-  const handleTerminalClick = (e) => {
-    console.log(e.target.id);
+  const handleTerminalClick = (file) => {
+    if (file === "AboutMe.txt") {
+      const aboutMe =
+        "Hey! Thanks for stopping by in the terminal and checking out the about me. Obviously, I love to be creative in my development work. In my free time, I love to cook and bake and am a hobby musician. I love to meet new people so feel free to drop me a line whenever!";
+      setTerminalReturn([...terminalReturn, aboutMe]);
+    } else if (file.includes(".com")) {
+      window.open(
+        "mailto:mattmarsdendev@gmail.com?subject=Wow, this terminal is really neat!"
+      );
+    } else if (file.includes(".io")) {
+      window.open("/#/portfolio");
+    }
   };
 
   useEffect(() => {}, [terminalReturn]);
 
+  const handleFileLink = (item) => {
+    const file = item.target.id;
+    console.log(file);
+    if (file.includes("txt") || file.includes(".com") || file.includes(".io"))
+      handleTerminalClick(file);
+  };
+
   const terminalReturnHTML = terminalReturn.map((item) => (
     <div
-      className="terminal-return"
-      onClick={item.includes("txt") ? handleTerminalClick : null}
+      className={
+        item.includes("txt") || item.includes(".com") || item.includes(".io")
+          ? "terminal-return txt-link"
+          : "terminal-return"
+      }
+      onClick={(item) => handleFileLink(item)}
       id={item}
     >
       {console.log(item)}
@@ -84,11 +134,15 @@ const Welcome = () => {
         </div>
         <div className="tool-text-container">
           <div className="tools-link" onClick={handleAboutLink}>
-            AboutMe
+            $AboutMe
           </div>
-          <div className="tools-link">Contact</div>
-          <div className="tools-link">Portfolio</div>
-          <div className="tools-link">Tools</div>
+          <div className="tools-link" onClick={handleContactLink}>
+            $Contact
+          </div>
+          <div className="tools-link" onClick={handlePortfolioLink}>
+            $Portfolio
+          </div>
+          <div className="tools-link">$Tools</div>
         </div>
         <div className="termain-return-container">{terminalReturnHTML}</div>
         <form onSubmit={handleSubmit}>
