0f4c2bbd37376b05ae2e6ce0c3cc7dc5899e157b
mmarsden89 <mattmarsdendev@gmail.com>
2021-07-01 20:46:15 -0400

    add primitive contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import "./Contact.scss";
+
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="contact-container">
+      <FontAwesomeIcon icon={faCoffee} />
+    </div>
+  );
 };
 
 export default Contact;
